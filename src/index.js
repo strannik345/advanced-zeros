f05c47c8383329f423ac21579bf4a2ff3d0f6627
strannik345 <volodkovasya@gmail.com>
2019-02-22 19:34:29 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let primes=[];
+  for(let i=2;base>1;)
+  {
+    if(base%i===0)
+    {
+      primes.push(i);
+      base=base/i;
+    }
+    else
+    {
+      if(i>2) i+=2;
+      else i=3;
+    }
+  }
+  const uniquePrimes=[...new Set(primes)];
+  const countPrimes=uniquePrimes.map(function(pr){
+      let answer=0;
+      for(let i=1;Math.pow(pr,i)<=number;i++)
+      {
+        answer+=parseInt(number/Math.pow(pr,i));  
+      }
+      return answer;
+  });
+  let count = [];
+  for(let i=0;i<countPrimes.length;i++)
+  {
+    count.push(Math.floor(countPrimes[i]/primes.filter(a=>a==uniquePrimes[i]).length));
+  }
+  console.log(count);
+  return Math.min(...count);
 }
